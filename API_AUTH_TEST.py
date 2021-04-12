e0a7ec4d3ad3908e2ff493f68a578468deac96fc
David Dekanozishvili <dekanozishvili@gmail.com>
2021-04-12 15:39:10 +0400

    baisic auth left for test

--- API_AUTH_TEST.py
+++ API_AUTH_TEST.py
@@ -1,8 +1,27 @@
 
-from logging import error
+# from logging import error
 import requests
 import urllib.parse as uparse
 import config
+
+import logging
+
+# Enabling debugging at http.client level (requests->urllib3->http.client)
+# you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# the only thing missing will be the response.body which is not logged.
+try: # for Python 3
+    from http.client import HTTPConnection
+except ImportError:
+    from httplib import HTTPConnection
+HTTPConnection.debuglevel = 1
+
+logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+
 
 # curl -H'Authorization: cpanel username:APITOKEN' 'https://example.com:2083/execute/Module/function?parameter=value'
 
@@ -11,21 +30,20 @@ session = requests.session()
 credentials = f'{config.USERNAME}:{config.API}'
 headers = {
     'Authorization': f'cpanel {credentials}',
-    'User-Agent': config.DEFAULT_USER_AGENT
+    'User-Agent': config.DEFAULT_USER_AGENT,
+    'Accept': 'application/json'
 }
 session.headers.update(headers)
 
 base_url = '{}://{}:{}'.format('https', config.URL, config.PORT)
-module = 'SSL'
-function = 'fetch_certificates_for_fqdns'
+module = 'Tokens'
+function = 'list'
 path = f'/execute/{module}/{function}'
 url = uparse.urljoin(base_url, path)
 print(url)
 
-domain = input('Enter domain to get SSL:')
-params = {
-    'domains': domain
-}
+
+params = {}
 
 r = session.post(url, params=params)
 
@@ -33,10 +51,10 @@ print(r.headers['Content-Type'])
 print(r.status_code)
 
 if r.status_code == 401:
-    raise error
+    raise requests_log.error
 try:
     print(r.json())
 except ValueError:
-    raise error
+    raise requests_log.error
 
 print(r.text)
---
+++ baisic_auth.py
@@ -0,0 +1,29 @@
+from base64 import b64encode
+import requests
+import urllib.parse as uparse
+import config
+
+
+session = requests.session()
+session.headers.update({'User-Agent': config.DEFAULT_USER_AGENT})
+
+
+credentials = f'{config.USERNAME}:{config.PASSWORD}'
+enc = b64encode(credentials.encode()).decode()
+auth = f'Basic {enc}'
+
+module = 'SSL'  # get module TEST
+function = 'fetch_certificates_for_fqdns'  # get function TEST
+
+domain = input('Enter domain to get SSL:')
+base_url = '{}://{}:{}'.format('https', config.URL, config.PORT)
+path = f'/execute/{module}/{function}'
+params = {
+    'domains': domain
+}
+url = uparse.urljoin(base_url, path)
+
+headers = {'Authorization': auth}
+print(headers)
+
+r = session.post(url, params, headers=headers)
