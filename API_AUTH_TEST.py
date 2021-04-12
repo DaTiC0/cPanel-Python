3de50bd3375d2028bec07b3292f276edec43bc2b
David Dekanozishvili <dekanozishvili@gmail.com>
2021-04-12 15:06:16 +0400

    update headers

--- API_AUTH_TEST.py
+++ API_AUTH_TEST.py
@@ -7,26 +7,30 @@ import config
 # curl -H'Authorization: cpanel username:APITOKEN' 'https://example.com:2083/execute/Module/function?parameter=value'
 
 session = requests.session()
-session.headers.update({'User-Agent': config.DEFAULT_USER_AGENT})
+
 credentials = f'{config.USERNAME}:{config.API}'
-print(credentials)
 headers = {
     'Authorization': f'cpanel {credentials}',
+    'User-Agent': config.DEFAULT_USER_AGENT
 }
-base_url = '{}://{}:{}'.format('https', config.URL, config.PORT)
+session.headers.update(headers)
 
+base_url = '{}://{}:{}'.format('https', config.URL, config.PORT)
 module = 'SSL'
 function = 'fetch_certificates_for_fqdns'
-
 path = f'/execute/{module}/{function}'
 url = uparse.urljoin(base_url, path)
 print(url)
+
 domain = input('Enter domain to get SSL:')
 params = {
     'domains': domain
 }
 
-r = session.post(url, headers=headers, params=params)
+r = session.post(url, params=params)
+
+print(r.headers['Content-Type'])
+print(r.status_code)
 
 if r.status_code == 401:
     raise error
