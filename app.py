b08c9e09f65fd6420aa1228ce4f24585674bd177
David Dekanozishvili <dekanozishvili@gmail.com>
2021-04-12 15:09:20 +0400

    change auth from base to token

--- app.py
+++ app.py
@@ -2,34 +2,32 @@
 # TESTING
 
 from logging import error
-from base64 import b64encode
+
 import requests
 import urllib.parse as uparse
 import config
 
 
 session = requests.session()
-session.headers.update({'User-Agent': config.DEFAULT_USER_AGENT})
 
+credentials = f'{config.USERNAME}:{config.API}'
+headers = {
+    'Authorization': f'cpanel {credentials}',
+    'User-Agent': config.DEFAULT_USER_AGENT
+}
+session.headers.update(headers)
 
-credentials = f'{config.USERNAME}:{config.PASSWORD}'
-enc = b64encode(credentials.encode()).decode()
-auth = f'Basic {enc}'
-####################################
-# FOR TEST #
-module = 'SSL'  # get module TEST
-function = 'fetch_certificates_for_fqdns'  # get function TEST
+base_url = '{}://{}:{}'.format('https', config.URL, config.PORT)
+module = 'SSL'
+function = 'fetch_certificates_for_fqdns'
+path = f'/execute/{module}/{function}'
+url = uparse.urljoin(base_url, path)
+print(url)
 
 domain = input('Enter domain to get SSL:')
-base_url = '{}://{}:{}'.format('https', config.URL, config.PORT)
-path = f'/execute/{module}/{function}'
 params = {
     'domains': domain
 }
-url = uparse.urljoin(base_url, path)
-
-headers = {'Authorization': auth}
-print(headers)
 
 r = session.post(url, params, headers=headers)
 
